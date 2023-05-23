4c63112b49aa0ce6f96f82f07fcff204010237b8
Katie Hughes <katiereeshughes@gmail.com>
2023-05-23 13:12:49 -0500

    Gitignore

--- unitree_crowd_nav/src/filter_point_cloud.cpp
+++ unitree_crowd_nav/src/filter_point_cloud.cpp
@@ -18,27 +18,33 @@ class FilterPC : public rclcpp::Node
       publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
       pc_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
         "rslidar_points", 10, std::bind(&FilterPC::pc_cb, this, std::placeholders::_1));
-      timer_ = create_wall_timer(
-      500ms, std::bind(&FilterPC::timer_callback, this));
+      // timer_ = create_wall_timer(
+      // 500ms, std::bind(&FilterPC::timer_callback, this));
     }
 
   private:
 
-    rclcpp::TimerBase::SharedPtr timer_;
+    // rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pc_sub_;
     size_t count_;
 
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
+    // void timer_callback()
+    // {
+    //   auto message = std_msgs::msg::String();
+    //   message.data = "Hello, world! " + std::to_string(count_++);
+    //   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    //   publisher_->publish(message);
+    // }
     void pc_cb(const sensor_msgs::msg::PointCloud2 & msg)
     {
-      RCLCPP_INFO_STREAM(get_logger(), "\n\nPoint Cloud Received");
+      const auto height = msg.height;
+      const auto width = msg.width;
+      const auto is_bigendian = msg.is_bigendian;
+      const auto point_step = msg.point_step;
+      const auto row_step = msg.row_step;
+      const auto data = msg.data;
+      RCLCPP_INFO_STREAM(get_logger(), "Width: "<<width<<" Height: "<<height);
     }
 };
 
