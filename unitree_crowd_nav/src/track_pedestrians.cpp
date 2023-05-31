7c7bf72b6d45ee752bb1934df3bade16aa09c9a8
Katie Hughes <katiereeshughes@gmail.com>
2023-05-31 13:43:23 -0500

    Add camera config files. At some point they should go into unitree camera package.

--- unitree_crowd_nav/src/track_pedestrians.cpp
+++ unitree_crowd_nav/src/track_pedestrians.cpp
@@ -6,25 +6,36 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "image_geometry/stereo_camera_model.h"
+#include "sensor_msgs/msg/camera_info.hpp"
 
 using namespace std::chrono_literals;
 
-class FilterPC : public rclcpp::Node
+class TrackPedestrians : public rclcpp::Node
 {
   public:
-    FilterPC()
-    : Node("filter_point_cloud"), count_(0)
+    TrackPedestrians()
+    : Node("track_pedestrians"), count_(0)
     {
+
+      auto pd = rcl_interfaces::msg::ParameterDescriptor{};
+
+      pd.description = "Timer rate (Hz)";
+      declare_parameter("rate", 200., pd);
+
+      rate_hz = get_parameter("rate").as_double();
+      RCLCPP_INFO_STREAM(get_logger(), "Rate is " << ((int)(1000. / rate_hz)) << "ms");
+      std::chrono::milliseconds rate = (std::chrono::milliseconds) ((int)(1000. / rate_hz));
       publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
       // pc_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
-      //   "rslidar_points", 10, std::bind(&FilterPC::pc_cb, this, std::placeholders::_1));
+      //   "rslidar_points", 10, std::bind(&TrackPedestrians::pc_cb, this, std::placeholders::_1));
       timer_ = create_wall_timer(
-      500ms, std::bind(&FilterPC::timer_callback, this));
+      rate, std::bind(&TrackPedestrians::timer_callback, this));
     }
 
   private:
-
+    double rate_hz;
     rclcpp::TimerBase::SharedPtr timer_;
+    image_geometry::StereoCameraModel stereo;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     // rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pc_sub_;
     size_t count_;
@@ -41,7 +52,7 @@ class FilterPC : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<FilterPC>());
+  rclcpp::spin(std::make_shared<TrackPedestrians>());
   rclcpp::shutdown();
   return 0;
 }
