c1605691786588cfbd9e1803dcd17d5111618797
Katie Hughes <katiereeshughes@gmail.com>
2023-10-09 15:06:48 -0500

    controller structure

--- crowd_nav/src/controller.cpp
+++ crowd_nav/src/controller.cpp
@@ -8,8 +8,7 @@
 #include "image_geometry/stereo_camera_model.h"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "crowd_nav_interfaces/msg/pixel.hpp"
-#include "crowd_nav_interfaces/msg/pixel_array.hpp"
+#include "crowd_nav_interfaces/msg/twist_array.hpp"
 #include <opencv2/highgui.hpp>
 #include "visualization_msgs/msg/marker.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
@@ -18,7 +17,7 @@
 
 using namespace std::chrono_literals;
 
-class PathPlan : public rclcpp::Node
+class Controller : public rclcpp::Node
 {
   public:
     Controller()
@@ -34,13 +33,13 @@ class PathPlan : public rclcpp::Node
       RCLCPP_INFO_STREAM(get_logger(), "Rate is " << ((int)(1000. / rate_hz)) << "ms");
       std::chrono::milliseconds rate = (std::chrono::milliseconds) ((int)(1000. / rate_hz));
 
-      people_sub_ = create_subscription<visualization_msgs::msg::MarkerArray>(
-        "pedestrians", 10, std::bind(&PathPlan::pedestrians_cb, this, std::placeholders::_1));
+      cmd_buf_sub_ = create_subscription<crowd_nav_interfaces::msg::TwistArray>(
+        "cmd_buf", 10, std::bind(&Controller::cmd_buf_cb, this, std::placeholders::_1));
 
       cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
       timer_ = create_wall_timer(
-      rate, std::bind(&PathPlan::timer_callback, this));
+      rate, std::bind(&Controller::timer_callback, this));
 
     }
 
@@ -48,25 +47,27 @@ class PathPlan : public rclcpp::Node
     double rate_hz;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr people_sub_;
+    rclcpp::Subscription<crowd_nav_interfaces::msg::TwistArray>::SharedPtr cmd_buf_sub_;
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
 
-    void pedestrians_cb(const visualization_msgs::msg::MarkerArray & msg)
+    void cmd_buf_cb(const crowd_nav_interfaces::msg::TwistArray & msg)
     {
-      RCLCPP_INFO_STREAM(get_logger(), "Received pedestrians");
+      RCLCPP_INFO_STREAM(get_logger(), "Received buffer.");
     }
 
     void timer_callback()
     {
-      RCLCPP_INFO_STREAM(get_logger(), "Timer tick");
+      // RCLCPP_INFO_STREAM(get_logger(), "Timer tick");
+      geometry_msgs::msg::Twist vel;
+      cmd_vel_pub_->publish(vel);
     }
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<PathPlan>());
+  rclcpp::spin(std::make_shared<Controller>());
   rclcpp::shutdown();
   return 0;
 }
