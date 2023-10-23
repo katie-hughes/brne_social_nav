4f70a24b0f5de9ea47638af95b182bdd76c0851b
Katie <katiereeshughes@gmail.com>
2023-10-23 17:30:33 -0500

    convert pedestrian to odom frame

--- pedestrian_tracking/src/convert_zed_ped.cpp
+++ pedestrian_tracking/src/convert_zed_ped.cpp
@@ -34,16 +34,16 @@ class ConvertPeds : public rclcpp::Node
       // Writing-A-Tf2-Broadcaster-Cpp.html
       tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 
-      // tf_buffer_= std::make_unique<tf2_ros::Buffer>(this->get_clock());
-      // tf_listener_= std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+      tf_buffer_= std::make_unique<tf2_ros::Buffer>(this->get_clock());
+      tf_listener_= std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
     }
 
   private:
     double rate_hz;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    // std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    // std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
@@ -58,49 +58,33 @@ class ConvertPeds : public rclcpp::Node
       // RCLCPP_INFO_STREAM(get_logger(), "\n\nMESSAGE");
       for (int i = 0; i < n_peds; i++){
         const auto p = msg.objects.at(i);
-        // RCLCPP_INFO_STREAM(get_logger(), "id "<<p.label_id);
-        // RCLCPP_INFO_STREAM(get_logger(), "pos:"   <<p.position.at(0)<<" "<<p.position.at(1)<<" "<<p.position.at(2));
-        // TODO: These positions are in zed frame. Need to be converted into odom frame.
+        // These positions are in zed frame. Need to be converted into odom frame.
         // frame id is always zed2i_left_camera_frame
+        const std::string ped_name = "ped_"+std::to_string(p.label_id);
         geometry_msgs::msg::TransformStamped t;
         t.header.stamp = msg.header.stamp;
         t.header.frame_id = "zed2i_left_camera_frame";
-        t.child_frame_id = "ped_"+std::to_string(p.label_id);
+        t.child_frame_id = ped_name;
         t.transform.translation.x = p.position.at(0);
         t.transform.translation.y = p.position.at(1);
         t.transform.translation.z = p.position.at(2);
         // Send the transformation
         tf_broadcaster_->sendTransform(t);
-
-        // crowd_nav_interfaces::msg::Pedestrian ped;
-        // ped.id = p.label_id;
-        // ped.pose.position.x = p.position.at(0);
-        // ped.pose.position.y = p.position.at(1);
-        // ped.pose.position.z = p.position.at(2);
-        // pa.pedestrians.push_back(ped);
-        
-        // Look up: T_odom_camera
-        // message gives T_camera_ped
-        // then I get T_odom_camera * T_camera_ped = T_odom_ped which is what message should contain.
-        // or i could just do a tf frame for each person tbh this might be the way to go.
-        // geometry_msgs::msg::TransformStamped T_odom_camera;
-        // try {
-        //   T_odom_camera = tf_buffer_->lookupTransform(
-        //     "zed2i_left_camera_frame", "odom",
-        //     tf2::TimePointZero);
-        //   geometry_msgs::msg::TransformStamped T_camera_ped;
-        //   T_camera_ped.header.frame_id = "zed2i_left_camera_frame";
-        //   T_camera_ped.header.stamp = T_odom_camera.header.stamp;
-        //   T_camera_ped.child_frame_id = "ped";
-        //   T_camera_ped.transform.translation.x = p.position.at(0);
-        //   T_camera_ped.transform.translation.y = p.position.at(1);
-        //   T_camera_ped.transform.translation.z = p.position.at(2);
-
-        //   tf2::Transform t1 = T_odom_camera;
-        //   tf2::Transform t2;
-        // } catch (const tf2::TransformException & ex) {
-        //   RCLCPP_INFO_STREAM(get_logger(), "Could not transform");
-        // }
+        // immediately read this tf to get the transform.
+        geometry_msgs::msg::TransformStamped T_odom_ped;
+        try {
+          T_odom_ped = tf_buffer_->lookupTransform(
+            "odom", ped_name,
+            tf2::TimePointZero);
+          crowd_nav_interfaces::msg::Pedestrian ped;
+          ped.id = p.label_id;
+          ped.pose.position.x = T_odom_ped.transform.translation.x;
+          ped.pose.position.y = T_odom_ped.transform.translation.y;
+          ped.pose.position.z = T_odom_ped.transform.translation.z;
+          pa.pedestrians.push_back(ped);
+        } catch (const tf2::TransformException & ex) {
+          RCLCPP_INFO_STREAM(get_logger(), "Could not transform");
+        }
       }
       pedestrian_pub_->publish(pa);
     }
