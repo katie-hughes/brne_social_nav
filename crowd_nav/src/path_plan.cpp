c557b5c40b9209f94f16cb655b53b3f51e3b89fc
Katie Hughes <katiereeshughes@gmail.com>
2023-11-02 14:57:18 -0500

    Clean up how I'm defining parameters

--- crowd_nav/src/path_plan.cpp
+++ crowd_nav/src/path_plan.cpp
@@ -23,74 +23,61 @@ class PathPlan : public rclcpp::Node
     PathPlan()
     : Node("plan_paths")
     {
+      // define parameters
+      declare_parameter("replan_freq", 1.0);
+      declare_parameter("dt", 0.1);
+      declare_parameter("maximum_agents", 1);
+      declare_parameter("n_samples", 1);
+      declare_parameter("n_steps", 1);
+      declare_parameter("cost_a1", 1.0);
+      declare_parameter("cost_a2", 1.0);
+      declare_parameter("cost_a3", 1.0);
+      declare_parameter("kernel_a1", 1.0);
+      declare_parameter("kernel_a2", 1.0);
+      declare_parameter("y_min", -1.0);
+      declare_parameter("y_max", 1.0);
+      declare_parameter("max_ang_vel", 1.0);
 
-      auto pd = rcl_interfaces::msg::ParameterDescriptor{};
-
-      pd.description = "Timer rate (Hz)";
-      declare_parameter("replan_freq", 1.0, pd);
-      pd.description = "dt";
-      declare_parameter("dt", 0.1, pd);
-      pd.description = "Maximum agents BRNE will consider";
-      declare_parameter("maximum_agents", 1, pd);
-      pd.description = "Number of samples for BRNE optimization";
-      declare_parameter("n_samples", 1, pd);
-      pd.description = "Number of timesteps for BRNE optimization";
-      declare_parameter("n_steps", 1, pd);
-      pd.description = "cost a1";
-      declare_parameter("cost_a1", 1.0, pd);
-      pd.description = "cost a2";
-      declare_parameter("cost_a2", 1.0, pd);
-      pd.description = "cost a3";
-      declare_parameter("cost_a3", 1.0, pd);
-      pd.description = "kernel_a1";
-      declare_parameter("kernel_a1", 1.0, pd);
-      pd.description = "kernel_a2";
-      declare_parameter("kernel_a2", 1.0, pd);
-      pd.description = "minimum y coordinate of cooridor";
-      declare_parameter("y_min", -1.0, pd);
-      pd.description = "maximum y coordinate of cooridor";
-      declare_parameter("y_max", 1.0, pd);
-      pd.description = "maximum ang vel of robot";
-      declare_parameter("max_ang_vel", 1.0, pd);
-
-
+      // get parameters
       replan_freq = get_parameter("replan_freq").as_double();
-      RCLCPP_INFO_STREAM(get_logger(), "Replan frequency: " << replan_freq << " Hz");
       dt = get_parameter("dt").as_double();
-      RCLCPP_INFO_STREAM(get_logger(), "dt: " << dt);
       maximum_agents = get_parameter("maximum_agents").as_int();
-      RCLCPP_INFO_STREAM(get_logger(), "Maximum agents: " << maximum_agents);
       n_samples = get_parameter("n_samples").as_int();
-      RCLCPP_INFO_STREAM(get_logger(), "Number of samples: " << n_samples);
       n_steps = get_parameter("n_steps").as_int();
-      RCLCPP_INFO_STREAM(get_logger(), "Number of timesteps: " << n_steps);
       cost_a1 = get_parameter("cost_a1").as_double();
       cost_a2 = get_parameter("cost_a2").as_double();
       cost_a3 = get_parameter("cost_a3").as_double();
-      RCLCPP_INFO_STREAM(get_logger(), "Costs: " << cost_a1 << " " << cost_a2 << " " << cost_a3);
       kernel_a1 = get_parameter("kernel_a1").as_double();
       kernel_a2 = get_parameter("kernel_a2").as_double();
-      RCLCPP_INFO_STREAM(get_logger(), "Kernels: " << kernel_a1 << " " << kernel_a2);
       y_min = get_parameter("y_min").as_double();
       y_max = get_parameter("y_max").as_double();
+
+      // print out parameters
+      RCLCPP_INFO_STREAM(get_logger(), "Replan frequency: " << replan_freq << " Hz");
+      RCLCPP_INFO_STREAM(get_logger(), "dt: " << dt);
+      RCLCPP_INFO_STREAM(get_logger(), "Maximum agents: " << maximum_agents);
+      RCLCPP_INFO_STREAM(get_logger(), "Number of samples: " << n_samples);
+      RCLCPP_INFO_STREAM(get_logger(), "Number of timesteps: " << n_steps);
+      RCLCPP_INFO_STREAM(get_logger(), "Costs: " << cost_a1 << " " << cost_a2 << " " << cost_a3);
+      RCLCPP_INFO_STREAM(get_logger(), "Kernels: " << kernel_a1 << " " << kernel_a2);
       RCLCPP_INFO_STREAM(get_logger(), "Hallway: " << y_min << "->" << y_max);
 
-      
       brne = brne::BRNE{kernel_a1, kernel_a2,
                         cost_a1, cost_a2, cost_a3,
                         dt, n_steps, n_samples,
                         y_min, y_max};
 
+      // Print out the parameters of the BRNE object to make sure it got initialized right
       RCLCPP_INFO_STREAM(get_logger(), brne.param_string());
 
+      // Define publishers and subscribers
       pedestrian_sub_ = create_subscription<crowd_nav_interfaces::msg::PedestrianArray>(
         "pedestrians", 10, std::bind(&PathPlan::pedestrians_cb, this, std::placeholders::_1));
-
       cmd_buf_pub_ = create_publisher<crowd_nav_interfaces::msg::TwistArray>("cmd_buf", 10);
 
+      // Create a timer that executes at replan_freq
       std::chrono::milliseconds rate = (std::chrono::milliseconds) ((int)(1000. / replan_freq));
-      timer_ = create_wall_timer(
-      rate, std::bind(&PathPlan::timer_callback, this));
+      timer_ = create_wall_timer(rate, std::bind(&PathPlan::timer_callback, this));
 
     }
 
