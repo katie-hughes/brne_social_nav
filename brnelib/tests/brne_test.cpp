e2df33cb45bb10489d0e3e936d61148993462093
Katie Hughes <katiereeshughes@gmail.com>
2023-11-27 00:47:54 -0600

    Add another test case

--- brnelib/tests/brne_test.cpp
+++ brnelib/tests/brne_test.cpp
@@ -92,27 +92,69 @@ namespace brne {
         xtraj_samples.submat(0, 0, n_samples-1, n_steps-1) = robot_xtraj_samples;
         ytraj_samples.submat(0, 0, n_samples-1, n_steps-1) = robot_ytraj_samples;
 
+        // =========================================================================================
+
         // pedestrian static at (1.0, 0.01)
         double ped_x = 1.0;
         double ped_y = 0.01;
         xtraj_samples.submat((1)*n_samples, 0, (2)*n_samples-1, n_steps-1) = arma::mat(n_samples, n_steps, arma::fill::value(ped_x));
         ytraj_samples.submat((1)*n_samples, 0, (2)*n_samples-1, n_steps-1) = arma::mat(n_samples, n_steps, arma::fill::value(ped_y));
 
-        auto weights = brne_test.brne_nav(xtraj_samples, ytraj_samples);
+        auto static_weights = brne_test.brne_nav(xtraj_samples, ytraj_samples);
 
-        std::cout << "Weights\n" << weights << std::endl;
+        std::cout << "Static Weights\n" << static_weights << std::endl;
 
-        REQUIRE(static_cast<int>(weights.n_rows) == n_agents);
-        REQUIRE(static_cast<int>(weights.n_cols) == n_samples);
+        REQUIRE(static_cast<int>(static_weights.n_rows) == n_agents);
+        REQUIRE(static_cast<int>(static_weights.n_cols) == n_samples);
 
-        arma::mat desired_weights(n_agents, n_samples, arma::fill::zeros);
+        arma::mat desired_static_weights(n_agents, n_samples, arma::fill::zeros);
         // hard-coded from a version of the algorithm that I know works
-        desired_weights.row(0) = arma::rowvec(std::vector<double>{7.1966e+00,8.8948e-01,6.0468e-02,3.5967e-03,2.7752e-04,3.3587e-05,6.5357e-06,1.9297e-06,6.9580e+00,8.3311e-01,5.4973e-02,3.2140e-03,2.4731e-04,3.0161e-05,5.9462e-06,1.7818e-06});
-        desired_weights.row(1) = arma::rowvec(n_samples, arma::fill::ones);
+        desired_static_weights.row(0) = arma::rowvec(std::vector<double>{7.1966e+00,8.8948e-01,6.0468e-02,3.5967e-03,2.7752e-04,3.3587e-05,6.5357e-06,1.9297e-06,6.9580e+00,8.3311e-01,5.4973e-02,3.2140e-03,2.4731e-04,3.0161e-05,5.9462e-06,1.7818e-06});
+        desired_static_weights.row(1) = arma::rowvec(n_samples, arma::fill::ones);
 
-        std::cout << "Desired Weights\n" << desired_weights << std::endl;
+        std::cout << "Desired Static Weights\n" << desired_static_weights << std::endl;
 
-        REQUIRE(arma::approx_equal(weights, desired_weights, "absdiff", 0.0001));
+        REQUIRE(arma::approx_equal(static_weights, desired_static_weights, "absdiff", 0.0001));
+
+        // =========================================================================================
+
+        // pedestrian moving at -0.5 m/s in x direction
+        double ped_vx = -0.5;
+        double ped_vy = 0.0;
+
+        arma::rowvec ped_xmean = arma::rowvec(n_steps, arma::fill::value(ped_x)) +
+          arma::linspace<arma::rowvec>(0, (n_steps - 1), n_steps) * dt * ped_vx;
+        arma::rowvec ped_ymean = arma::rowvec(n_steps, arma::fill::value(ped_y)) +
+          arma::linspace<arma::rowvec>(0, (n_steps - 1), n_steps) * dt * ped_vy;
+
+        arma::mat ped_xmean_mat(n_samples, n_steps, arma::fill::zeros);
+        arma::mat ped_ymean_mat(n_samples, n_steps, arma::fill::zeros);
+        ped_xmean_mat.each_row() = ped_xmean;
+        ped_ymean_mat.each_row() = ped_ymean;
+
+        const auto speed_factor = 1.0;
+
+        const auto ped_xsamples = arma::conv_to<arma::mat>::from(ped_xmean_mat + brne_test.mvn_sample_normal() * speed_factor);
+        const auto ped_ysamples = arma::conv_to<arma::mat>::from(ped_ymean_mat + brne_test.mvn_sample_normal() * speed_factor);
+
+        xtraj_samples.submat((1)*n_samples, 0, (2)*n_samples-1, n_steps-1) = ped_xsamples;
+        ytraj_samples.submat((1)*n_samples, 0, (2)*n_samples-1, n_steps-1) = ped_ysamples;
+
+        const auto moving_weights = brne_test.brne_nav(xtraj_samples, ytraj_samples);
+
+        std::cout << "Moving Weights\n" << moving_weights << std::endl;
+
+        REQUIRE(static_cast<int>(moving_weights.n_rows) == n_agents);
+        REQUIRE(static_cast<int>(moving_weights.n_cols) == n_samples);
+
+        arma::mat desired_moving_weights(n_agents, n_samples, arma::fill::zeros);
+        // hard-coded from a version of the algorithm that I know works
+        desired_moving_weights.row(0) = arma::rowvec(std::vector<double>{7.4404e+00,1.9497e+00,2.5198e-01,2.0412e-02,1.5153e-03,1.4078e-04,1.9137e-05,3.9156e-06,5.3604e+00,9.0309e-01,6.8430e-02,3.6351e-03,2.2290e-04,2.1225e-05,3.3945e-06,8.7086e-07});
+        desired_moving_weights.row(1) = arma::rowvec(std::vector<double>{1.1278e-02,1.3576e-07,1.7904e-07,1.8944e-07,1.8020e-07,5.1752e-03,1.4078e-07,1.4274e-07,1.1432e-05,1.2219e-06,1.4527e-07,2.3051e-07,2.3235e-01,1.3523e-07,1.5751e+01,3.4930e-05});
+
+        std::cout << "Desired Moving Weights\n" << desired_moving_weights << std::endl;
+
+        REQUIRE(arma::approx_equal(moving_weights, desired_moving_weights, "absdiff", 0.001));
     }
 
 }
